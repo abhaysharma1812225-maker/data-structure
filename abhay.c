bb3b1ac94a03295fb73ef829593d808b924ab4c1
abhaysharma1812225-maker <abhaysharma1812225@gmail.com>
2026-03-18 10:29:57 +0530

    Create abhay.c

---
+++ abhay.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int A[10][10], B[10][10], C[10][10];
+	int i,j,row,cols;
+	
+	printf("Enter rows and coloumns:");
+	scanf("%d %d,&rows,&cols);
+	
+	printf("Enter element of matrix A:\n");
+	for(i=0; i<rows;i++) {
+		for(j=0; j<cols; j++){
+	scanf("%d", &A[i][j]);
+}
+printf("Enter element of matrix B:\n");
+for(i=0; i< rows;i++) {
+	for(j=0; j< cols; j++) {
+		scanf("%d", &B [i][j]);
+}
+printf("\")
+	
+	
